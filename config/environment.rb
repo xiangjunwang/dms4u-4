9f508d8a59ab1a1f55383055bca970edc8c4237d
XiangJun Wang <xiangjunwang321@gmail.com>
2014-04-27 11:27:23 -0400

    Automatically created project

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Dms4u40::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dms4u40::Application.config.session_store :cookie_store, key: '_dms4u4_0_session'
